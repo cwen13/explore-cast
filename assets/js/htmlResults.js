f6934816acbc43dbc40e9304d8c5d9d1473684ff
cwen13 <cwenrich13@gmail.com>
2022-12-15 20:43:54 -0500

    have programmable resilt tiles set up

--- assets/js/htmlResults.js
+++ assets/js/htmlResults.js
@@ -3,6 +3,12 @@
  * are created in the same way and no default tile is used
  */
 
+let eR = {"title": "Big concert",
+	  "date": "2022-03-12",
+	  "pic": "./../images/wireframe-mockup.png",
+	  "sourec": "My head",
+	  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",}
+
 function buildTile (eventResults) {
   // Need to make componets
   // Need to nest them together
@@ -15,46 +21,52 @@ function buildTile (eventResults) {
   // -Source of event
 
   // build shell
-  let container = document.create("section");
-  continainer.setAttribute("class", "box");
-  let resultTile = document.create("article");
+  let container = document.createElement("section");
+  container.setAttribute("class", "box results");
+  let resultTile = document.createElement("article");
   resultTile.setAttribute("class","media");
 
   // build picture elements
   let ePicEl = document.createElement("img");
-  ePicEl.setAttribute("src", eventResults("picture"));
-  let figuseEl = document.createElement("figure");
+  ePicEl.setAttribute("src", eventResults["picture"]);
+  let figureEl = document.createElement("figure");
   figureEl.setAttribute("class", "image is-64x64")
   let picSectionEl = document.createElement("section");
   picSectionEl.setAttribute("class","media-left");
 
-  // assemble picture
+  // build title and datetime items
+  let eTitleEl = document.createElement("section");
+  let titleEl = document.createElement("strong")
+  titleEl.textContent = eventResults["title"] + "<br></br>" + eventResults["date"]
+  eTitleEl.setAttribute("class", "colulmn is-fifth");
+  let titleSectionEl = document.createElement("section");
+  titleSectionEl.setAttribute("class", "content columns");
+  let middleSection = document.createElement("section");
+  middleSection.setAttribute("class", "media-content");
+  
+  let eDescEl = document.createElement("section");
+  eDescEl.setAttribute("class" , "column is-four-fifths");
+  eDescEl.textContent = eventResults["description"];
+    
+  let eSourceEl = document.createElement("a");
+  eSourceEl.textContent = eventResults["source"];
+
+  // build tile
   figureEl.appendChild(ePicEl);
   picSectionEl.appendChild(figureEl);
-  resultTile.appendChild(picSectionEl);
 
-  // build title and datetime items
-  let eTitleEl = document.crreateElement("section");
-  eTitleEl.textContent = eventResults("title");
-  
-  let eDateTimeEl = document.createElement("section");
-  let date = Date.parse(eventResults("dateTime"))
-  eTitleEl.textContent = date.toUTCString();
-  let titleSectionEl = document.createElement("section");
-  titleSectionEl.setAttribute("class", "content");
-  let middleSection = document.createElement("div");
-  middleSection.setAttribute("class", "media-content");
-  titleSectionEl.appendChild(eDateTimeEl);
-  titleSectionEl.insertBefore(eTitleEl, titleSectionEl[0]);
+  eTitleEl.appendChild(titleEl);
+  titleSectionEl.appendChild(eTitleEl);
+  titleSectionEl.appendChild(eDescEl);
+
   middleSection.appendChild(titleSectionEl);
   
-  let eDescrEl = document.createEement("section");
-  eDescEl.textContent = eventResults("description");
-  
-  let eSourceEl = document.createElement("a");
-  eSourceEl.textContent = eventResults("source");
+  resultTile.appendChild(middleSection);
+  resultTile.insertBefore(picSectionEl, middleSection);
 
-  
-  
+  let resultList = document.querySelector(".results");
+  resultList.appendChild(resultTile);
 
 }
+
+buildTile(eR);
