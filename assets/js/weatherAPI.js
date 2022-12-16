52b102b211aca74e969768cd3de47e8df4875587
zzurawski <116835382+zzurawski@users.noreply.github.com>
2022-12-15 21:30:53 -0500

    work in progress fetch

--- assets/js/weatherAPI.js
+++ assets/js/weatherAPI.js
@@ -1,5 +1,27 @@
 //Open Weather API
 //{API key}
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=7c5174592c22f511659949315b1dc206')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+const apiURL1 = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+const apiURL2 = '&appid=7c5174592c22f511659949315b1dc206';
+
+let userSubmit = document.getElementsByClassName('submit button is-primary');
+
+function getFetch() {
+
+userInput = document.getElementById('location').value;
+userLocation = JSON.stringify(userInput, 'replacer');
+
+//userLocation = 'Cleveland'
+
+completeURL = apiURL1 + userInput + apiURL2;
+
+
+
+    console.log('balls');
+    console.log(completeURL);
+    
+    fetch(completeURL) 
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+}
+
+userSubmit[0].addEventListener('click', getFetch());
