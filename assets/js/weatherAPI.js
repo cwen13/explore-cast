f2cd5e94aa99b7f5b549916c65184aac4aa7deb2
zzurawski <116835382+zzurawski@users.noreply.github.com>
2022-12-27 18:14:53 -0500

    failing still to put input from textbox as query

--- assets/js/weatherAPI.js
+++ assets/js/weatherAPI.js
@@ -1,27 +1,41 @@
 //Open Weather API
-//{API key}
 const apiURL1 = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+
+//{API key}
 const apiURL2 = '&appid=7c5174592c22f511659949315b1dc206';
+
+//empty array to fill user input for city
+let city = [];
+
+
+var userInput = document.getElementById('location').innerHTML;
+
 
 let userSubmit = document.getElementsByClassName('submit button is-primary');
 
+//handles click output
+userSubmit[0].addEventListener('click', getFetch());
+
+
 function getFetch() {
 
-userInput = document.getElementById('location').value;
-userLocation = JSON.stringify(userInput, 'replacer');
+    localStorage.setItem('city', userInput);
 
-//userLocation = 'Cleveland'
+    //city.push(userInput);
+    localStorage.getItem("city", JSON.stringify(userInput));
 
-completeURL = apiURL1 + userInput + apiURL2;
+    let completeAPI = apiURL1 + userInput + apiURL2;
 
-
-
-    console.log('balls');
-    console.log(completeURL);
-    
-    fetch(completeURL) 
-    .then((response) => response.json())
-    .then((data) => console.log(data))
+        console.log('balls');
+        console.log(completeAPI);
+        
+        fetch(completeAPI) 
+            .then((response) => response.json())
+            .then((data) => {
+            console.log(data);
+            localStorage.setItem('weatherResponse', JSON.stringify(data['list']));
+        })
+        
+    return
 }
 
-userSubmit[0].addEventListener('click', getFetch());
